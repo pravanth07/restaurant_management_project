e2b09a4bb51223b2c7655d26875b2d4ce04d60b7
pravanth07 <thumma772005@gmail.com>
2025-08-12 19:35:15 +0530

    28529

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "home.html", {
+        "restaurant_name= malnadu": settings.restaurant_name= malnadu
+    })
+    def about(request):
+        return render(request, "about.html", {
+            "restaurant_name=malnadu": settings.restaurant_name=malnadu,
+            "desription": "At Tasty Bites, we bring you fresh, flavorful meals"
+        
+        })
