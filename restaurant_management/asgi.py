0281f17c40cd75078191be50fa3ec54a866af9e7
pravanth07 <thumma772005@gmail.com>
2025-08-13 07:14:21 +0530

    28526

--- restaurant_management/asgi.py
+++ restaurant_management/asgi.py
@@ -14,3 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
 
 application = get_asgi_application()
+
+DEBUG = False
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "your-domain.com"]
