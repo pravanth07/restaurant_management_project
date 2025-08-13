899119a8e0988455886414b09af663e27df222bf
pravanth07 <thumma772005@gmail.com>
2025-08-13 07:12:35 +0530

    28526

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
+from django.contrib import admin
 from django.urls import path
-from .views import *
+
+
+handler404 = "yourapp.views.custom_404"
+
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     
 ]
