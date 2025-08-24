dc56004dc4676270e2f701d67613a6166fcfa002
pravanth07 <thumma772005@gmail.com>
2025-08-24 10:22:02 +0530

    36788

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+app = flask(___name__)
+
+@app.context_processor
+def inject_current_year():
+    return {"current_year": datetime.now().year}
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/reservations")
+def reservations():
+    return render_template("reservations.html")
