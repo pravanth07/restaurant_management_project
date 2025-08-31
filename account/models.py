b42c4695d09f6f6760199a9a5916db43dc84e5c0
pravanth07 <thumma772005@gmail.com>
2025-08-31 11:48:29 +0530

    38419

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Feedback(models.Model):
+    comment = models.TexttField()
+    created_at = model.DateTimeFIeld(auto_now_add=True)
+
+    def___str___(self):
+        return self.comment[:50]
+
 # Create your models here.
