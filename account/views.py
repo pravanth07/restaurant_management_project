f0cc8257e3fe8a709d087073254274f4c9c7df11
pravanth07 <thumma772005@gmail.com>
2025-09-02 20:56:48 +0530

    41059

--- account/views.py
+++ account/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class MenuView(APIView):
+    def get(self, request):
+        menu = [
+            {"name": "Margherita Pizza", "description": "Classic cheese and tomato pizza", "price": 8.99},
+            {"name": "Veggie Burger", "description": "Grilled veggie patty with lettuce and tomato", "price":7.49},
+            {"name": "pasta Alfredo", "description": "Creamy Alfredo sauce with fettuccine pasta":10.99},
+            {"name": "Caesar Salad", "description": "Fresh romaine lettuce with Caesar dressing","price":6.99},
+
+        ]
+
 
 def custom_404(request, exception):
     return render(request, "404.html", status=404)
     
+return Response(menu)
