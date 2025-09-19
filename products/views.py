5c28f8974e10b9a9ddaf97c3895e2c7c3a825a0f
pravanth07 <thumma772005@gmail.com>
2025-09-19 07:22:38 +0530

    43038

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from .forms import contactForm
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -24,3 +24,15 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def home(request):
+    if request.method == "POST":
+        form = contactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+        else:
+            form = contactForm()
+
+        return render(request, 'home.html', {'form': form})
