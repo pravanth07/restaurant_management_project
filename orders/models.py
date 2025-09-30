9aacc047900b6cf5f35742e9f5b4faaf1551a299
pravanth07 <thumma772005@gmail.com>
2025-09-30 08:38:55 +0530

    49104

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from .models import OrderStatus
 
+class order(models.Model):
+    # your existing fields...
+
+
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def__str__(self):
+        return f"order {self.id} - {self.status}"
 # Create your models here.
