90fc311db575afd9cb565e9766bae4eeabebfbd2
pravanth07 <thumma772005@gmail.com>
2025-10-09 07:25:27 +0530

    52581

--- orders/models.py
+++ orders/models.py
@@ -22,5 +22,14 @@ class OrderStatus(models.Model):
     def__str__(self):
         return self.name
 
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
 
+
+    def__str__(self):
+        return self.code 
 # Create your models here.
